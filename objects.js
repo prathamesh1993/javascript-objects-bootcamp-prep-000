5f18ae6c3e1729b968601b6e9bc624311b30aadb
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2018-10-02 06:53:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(the_playlist,name,song_name){
   the_playlist[name]=song_name
   return the_playlist
 }
+
+function removeFromPlaylist(playlist,name){
+  delete playlist.name
+  return playlist
+}
